a46be855931df1a176a9bfeb199c58497a9ef90d
Ajibola Ajani <ajaniajibola469@gmail.com>
2024-08-16 18:29:08 -0400

    Change drivetrain ports

--- src/main.cpp
+++ src/main.cpp
@@ -79,7 +79,7 @@ float GetCurveOutput(int input) {
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({-10, -9, -8});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup left_mg({-8, -9, -10});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({1, 2, 3});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
